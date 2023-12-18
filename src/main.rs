24a89e07bb3ad9f876ac1f98a84fb22a7b3d8c01
Serhii Ovchynnikov <unknown.musician.t@gmail.com>
2023-12-18 21:09:56 +0200

    Add time to logs and add timeout to reconnects

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,13 @@ use std::{
         // BufRead,
         Write,
     },
-    fs
+    fs,
+    thread,
+    time::{
+        Duration,
+        SystemTime,
+        UNIX_EPOCH
+    },
 };
 
 fn main() {
@@ -16,14 +22,13 @@ fn main() {
 
     for stream in listener.incoming() {
         let Ok(stream) = stream else {
-            eprintln!("Failed to establish connection.");
+            eprintln!("[{}] Failed to establish connection.", get_time());
+            thread::sleep(Duration::from_secs(2));
             continue;
         };
 
         handle_connection(stream);
     }
-
-    println!("Hello, world!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -38,7 +43,7 @@ fn handle_connection(mut stream: TcpStream) {
     let status_line = "HTTP/1.1 200 OK";
 
     let Ok(contents) = fs::read_to_string("content/hello.html") else {
-        eprintln!("Failed to read local files.");
+        eprintln!("[{}] Failed to read local files.", get_time());
         return;
     };
 
@@ -47,9 +52,29 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{status_line}{SEPARATOR}Content-Length: {length}{SEPARATOR}{SEPARATOR}{contents}");
 
     if let Err(error) = stream.write_all(response.as_bytes()) {
-        eprintln!("Error while writing to buffer: {error}");
+        eprintln!("[{}] Error while writing to buffer: {}", get_time(), error);
         return;
     }
 
-    println!("Answered with hello.html.")
+    println!("[{}] Answered with hello.html.", get_time());
+}
+
+fn get_time() -> String {
+    let mut since_the_epoch = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    let timezone_offset = Duration::from_secs(60 * 60 * 2);
+
+    since_the_epoch += timezone_offset;
+    
+    let total_seconds = since_the_epoch.as_secs();
+    let seconds_today = total_seconds % (60 * 60 * 24);
+    let seconds = seconds_today % 60;
+
+    let minutes_today = seconds_today / 60;
+    let minutes = minutes_today % 60;
+    let hours = minutes_today / 60;
+
+    format!("{hours:0>2}:{minutes:0>2}:{seconds:0>2}")
 }
